d9d65674d449eda7c6e9bd20665cb7c95cf94fb0
namlephuong95 <namlephuong95@gmail.com>
2020-05-07 20:08:39 +0700

    Web-Client

---
+++ WebServiceClient/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServiceClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ServiceReference2.Service1Client serviceClient = new ServiceReference2.Service1Client();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login(string code, string pass)
+        {
+            ServiceReference2.Account account = serviceClient.GetAccount(code, pass);
+            ViewBag.Name = account.Name;
+            ViewBag.Code = account.Code;
+            ViewBag.Money = account.Money;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Transfer(string sCode, string rCode, double amount)
+        {
+            var message = serviceClient.TransferMoney(sCode, rCode, amount);
+            ViewBag.Message = message;
+            return View();
+        }
+    }
+}
---
+++ WebServiceClient/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServiceClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //var serviceClient = new ServiceReference2.Service1Client();
+            //var std = new ServiceReference2.Student() {
+            //    Name = "Le Phuong Nam"
+            //};
+            //var result = serviceClient.GetStudentData(std);
+            //ViewBag.Message = "Your student name: " + result.Id;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
